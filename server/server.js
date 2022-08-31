df840cfbb4414526aa5ae55267b4651e8a99e6e2
Spencer Anderson <spenceranderson007@gmail.com>
2022-08-31 12:06:10 -0600

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+//endpoints
+app.get('/', (req, res) => {
+    //This is where we type the path
+    res.sendFile(path.join(__dirname, "../client/index/html"))
+})
+//This is how Heroku finds your html file^^^
+//__dirname(current directory name) is the location of server.js
+
+
+
+const port = 4005;
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
